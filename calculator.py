448f912e091a640098f2cdddebe593491ff63e55
ashrafcloud <65109004+ashrafcloud@users.noreply.github.com>
2020-08-01 22:25:39 +0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# Write a Program to pass two variable into a function and then perform arthemtic operations (+,-,*,/).print the result 
+def cal(a,b):
+    print("sum is:" ,a+b) 
+    print("diff is:", a-b)
+    print("product is:",a*b)
+    print("quotient is",a/b)
+    return()
+    
+
+x=int(input("enter a no 1: "))
+y=int(input("enter a no 2: "))
+cal(x,y)
+
+
+# Output
+enter a no 2: 2                                                                                                                         
+sum is: 6                                                                                                                               
+diff is: 2                                                                                                                              
+product is: 8                                                                                                                           
+quotient is 2.0                                                                                                                         
